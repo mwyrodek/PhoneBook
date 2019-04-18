07c5daebb277d5f30342690eaaa72aae29e1a801
Maciej Wyrodek <mwyrodek@objectivity.co.uk>
2019-04-18 15:41:26 +0200

    some experiments

---
+++ PhoneBook/PhoneBook/PhoneBook.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PhoneBook
+{
+    public class PhoneBook
+    {
+        public string EmployesFromLocation(string employee)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- PhoneBook/PhoneBook/Program.cs
+++ PhoneBook/PhoneBook/Program.cs
@@ -1,15 +1,28 @@
-﻿namespace PhoneBook
-{
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
+﻿using System;
+using System.Runtime.InteropServices;
 
+namespace PhoneBook
+{
     public class Program
     {
         public static void Main(string[] args)
         {
+            bool running = true;
+            var phoneBook = new PhoneBook();
+            while (running)
+            {
+                Console.WriteLine("What Do you want?");
+                var readLine = Console.ReadLine();
+                var comands = readLine.Split(' ');
+                switch (comands[0])
+                {
+                    case "Location":
+                            Console.WriteLine(value: phoneBook.EmployesFromLocation(comands[1]));
+                        break;
+                     default:
+                         throw new ArgumentOutOfRangeException();
+                }
+            }
         }
     }
 }
